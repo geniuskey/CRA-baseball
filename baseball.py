6d1e608ef9c5056c3097f2e6f47eddfb18003471
geniuskey <geniuskey@gmail.com>
2024-07-10 15:05:29 +0900

    교재에서 요구하는 것 모두 적용 완료

--- baseball.py
+++ baseball.py
@@ -7,18 +7,28 @@ class BaseballGame:
 
     def guess(self, guess_number: str):
         self.assert_illegal_value(guess_number)
-        if guess_number == self.question:
-            return GameResult(True, 3, 0)
+        if self.is_solved(guess_number):
+            return self.get_success_game_result()
         else:
-            strikes = 0
-            balls = 0
-            for i in range(len(self.question)):
-                if self.question.find(guess_number[i]) == i:
-                    strikes += 1
-                elif self.question.find(guess_number[i]) > -1:
-                    balls += 1
+            balls, strikes = self.get_unsolved_game_result(guess_number)
 
             return GameResult(False, strikes, balls)
+
+    def is_solved(self, guess_number):
+        return guess_number == self.question
+
+    def get_success_game_result(self):
+        return GameResult(True, 3, 0)
+
+    def get_unsolved_game_result(self, guess_number):
+        strikes = 0
+        balls = 0
+        for i in range(len(self.question)):
+            if self.question.find(guess_number[i]) == i:
+                strikes += 1
+            elif self.question.find(guess_number[i]) > -1:
+                balls += 1
+        return balls, strikes
 
     def assert_illegal_value(self, guess_number):
         if guess_number is None:
