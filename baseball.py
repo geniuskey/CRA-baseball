009e43b46309fb617f09853cda61cee6fb61a73a
geniuskey <geniuskey@gmail.com>
2024-07-10 13:54:57 +0900

    first commit

---
+++ baseball.py
@@ -0,0 +1,3 @@
+class Baseball:
+    def guess(self):
+        pass
---
+++ test_baseball.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestBaseball(TestCase):
+    def test_guess(self):
+        assert False
